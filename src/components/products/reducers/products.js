ad05410178ecfb6b6dea6621e5e4394bba44d44c
Pradosh Sinha <pradoshks@outlook.com>
2017-01-29 15:05:21 +1100

    Refactored reducers

--- src/components/products/reducers/products.js
+++ src/components/products/reducers/products.js
@@ -1,10 +1,18 @@
-export default function products(state = {}, action) {
+import {combineReducers} from 'redux'
+
+const products = (state = {}, action) => {
     switch (action.type) {
         case 'PRODUCTS_LIST':
             return action.products;
-
-        // initial state
         default:
-            return state;
+            return state
     }
+}
+
+export default combineReducers({
+    products
+})
+
+export const getProduct = (state, id) => {
+    return state.products.find((product) => {return product.id == id})
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,35 @@
-import { combineReducers } from "redux";
-import { routerReducer } from "react-router-redux";
-import { reducer as formReducer } from "redux-form";
+import {combineReducers} from "redux";
+import {routerReducer} from "react-router-redux";
+import {reducer as formReducer} from "redux-form";
 
-import products from "../components/products/reducers/products";
-import cart from "../components/cart/reducers/cart";
+import products, * as fromProducts from "../components/products/reducers/products";
+import cart, * as fromCart from "../components/cart/reducers/cart";
 
 
-// main reducers
 export const reducers = combineReducers({
-  routing: routerReducer,
-  form: formReducer,
-  products: products,
-  cart: cart
+    routing: routerReducer,
+    form: formReducer,
+    products,
+    cart
 });
+
+const getAddedIds = state => fromCart.getAddedIds(state.cart)
+const getQuantity = (state, id) => fromCart.getQuantity(state.cart, id)
+const getProduct = (state, id) => fromProducts.getProduct(state.products, id)
+
+export const getTotal = state => {
+    console.log('getTotal state', state);
+    return getAddedIds(state)
+        .reduce((total, id) =>
+            total + getProduct(state, id).price * getQuantity(state, id),
+            0
+        )
+        .toFixed(2)
+}
+
+
+export const getCartProducts = state =>
+    getAddedIds(state).map(id => ({
+        ...getProduct(state, id),
+        quantity: getQuantity(state, id)
+    }))
